269efc4b7b04677434bc3666b1faf289a8f451fd
Filip Zeman <zeman.filip@gmail.com>
2021-09-20 08:44:18 +0200

    -> update test (then, text, equal methods)

--- cypress/integration/web.spec.js
+++ cypress/integration/web.spec.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
 
-
 describe('Complex test suite', () => {
   beforeEach(() => {
 
@@ -10,6 +9,20 @@ describe('Complex test suite', () => {
 
   it('automatically fill in form and submit its data', () => {
 
-    cy.get('basic form')
+    cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+      const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+      const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+
+      expect(emailLabelFirst).to.equal('Email')
+      expect(passwordLabelFirst).to.equal('Password')
+
+      cy.contains('nb-card', 'Basic form').then(secondForm => {
+        const emailLabelSecond = secondForm.find('[for="exampleInputEmail1"]').text()
+        const passwordLabelSecond = secondForm.find('[for="exampleInputPassword1"]').text()
+      
+        // expect(emailLabelSecond).to.equal(emailLabelFirst)
+        expect(passwordLabelFirst).to.equal(passwordLabelSecond)
+      })
+    })    
   })
 })
